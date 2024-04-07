a79668519e07e1860b4e9cbbdc1b1e0bbd2325bf
svivek19 <svivek030503@gmail.com>
2024-04-07 17:26:12 +0530

    carousel fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,40 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { GrCaretPrevious } from "react-icons/gr";
 import { GrCaretNext } from "react-icons/gr";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
+
+import img1 from "../assets/1.jpg";
+import img2 from "../assets/2.jpg";
+import img3 from "../assets/3.jpg";
 
 const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [carouselImages, setCarouselImages] = useState([]);
-
-  useEffect(() => {
-    const fetchCarouselImages = async () => {
-      try {
-        const carouselCollection = collection(db, "carousel");
-        const snapshot = await getDocs(carouselCollection);
-        const images = snapshot.docs.map((doc) => doc.data());
-        console.log(images);
-        setCarouselImages(images);
-      } catch (error) {
-        console.error("Error fetching carousel images:", error);
-      }
-    };
-
-    fetchCarouselImages();
-  }, []);
 
   const goToPrevSlide = () => {
     setActiveIndex((prevIndex) =>
-      prevIndex === 0 ? carouselImages.length - 1 : prevIndex - 1
+      prevIndex === 0 ? items.length - 1 : prevIndex - 1
     );
   };
 
   const goToNextSlide = () => {
     setActiveIndex((prevIndex) =>
-      prevIndex === carouselImages.length - 1 ? 0 : prevIndex + 1
+      prevIndex === items.length - 1 ? 0 : prevIndex + 1
     );
   };
+
+  const items = [img1, img2, img3];
 
   return (
     <div
@@ -43,7 +30,7 @@ const Carousel = () => {
       data-carousel="static"
     >
       <div className="relative h-72 my-9 overflow-hidden rounded-lg md:h-[35rem]">
-        {carouselImages.map((image, index) => (
+        {items.map((item, index) => (
           <div
             key={index}
             className={`duration-700 ease-in-out ${
@@ -52,7 +39,7 @@ const Carousel = () => {
             data-carousel-item={index === activeIndex ? "active" : ""}
           >
             <img
-              src={image.url} // Assuming you have a 'url' field for the image URL in Firebase
+              src={item}
               className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               alt={`Slide ${index + 1}`}
             />
