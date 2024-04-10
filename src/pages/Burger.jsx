1b5c932e15c35d51d82e587b2f65a8d94a715256
svivek19 <svivek030503@gmail.com>
2024-04-10 08:57:59 +0530

    burger page complete

--- src/pages/Burger.jsx
+++ src/pages/Burger.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const Burger = () => {
-  return <div>Burger</div>;
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "burger"));
+      const data = querySnapshot.docs.map((doc) => doc.data());
+      setProduct(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="mt-28">
+      <div
+        className="w-11/12 md:w-5/6 mx-auto text-[#52321b]"
+        data-aos="fade-up"
+      >
+        <h1 className="text-4xl font-semibold mb-2">Burger</h1>
+        <p className="text-xl text-[#6e6a67]">
+          Cheesilicious Burgers to make every day extraordinary.
+        </p>
+      </div>
+      <Card product={product} />
+    </div>
+  );
 };
 
 export default Burger;
