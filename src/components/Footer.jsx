655e1f0e3bb9d01c27de74ae5154e7deccf21b7c
svivek19 <svivek030503@gmail.com>
2024-04-12 12:35:56 +0530

    bug fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,7 @@ function Footer() {
   return (
     <footer className="bg-[#412614] text-[#dad2cc]">
       <div className="w-full max-w-screen-xl mx-auto p-6 md:py-8">
-        <div
-          className="sm:flex sm:items-center sm:justify-between"
-          data-aos="fade-up"
-        >
+        <div className="sm:flex sm:items-center sm:justify-between">
           <a
             href="/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
