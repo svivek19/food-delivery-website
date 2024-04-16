e8de142d1b5d30e2b78dc1f425ec00ec0e1f83a4
svivek19 <svivek030503@gmail.com>
2024-04-16 11:47:27 +0530

    admin auth finish

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
+  const [adminMail, setAdminMail] = useState("");
+  const [adminPassword, setAdminPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const preEmail = "admin@gmail.com";
+  const prePass = "admin@123";
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    if (adminMail === preEmail && adminPassword === prePass) {
+      // navigate('')
+      alert("navigate");
+    } else {
+      toast.error("Only access for admin");
+      setAdminMail("");
+      setAdminPassword("");
+    }
+  };
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <Toaster />
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-[#52321b]">
           Admin Login
@@ -10,7 +33,7 @@ const Admin = () => {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleForm}>
           <div className="text-start">
             <label
               htmlFor="email"
@@ -26,6 +49,8 @@ const Admin = () => {
                 autoComplete="email"
                 placeholder="Enter your email"
                 required
+                value={adminMail}
+                onChange={(e) => setAdminMail(e.target.value)}
                 className="block w-full bg-slate-100 px-2 outline-none rounded-md border-0 py-1.5 text-[#52321b] shadow-sm placeholder:text-[#bebab7] sm:text-sm sm:leading-6"
               />
             </div>
@@ -47,6 +72,8 @@ const Admin = () => {
                 type="password"
                 autoComplete="current-password"
                 required
+                value={adminPassword}
+                onChange={(e) => setAdminPassword(e.target.value)}
                 placeholder="Enter your password"
                 className="block w-full bg-slate-100 px-2 outline-none rounded-md border-0 py-1.5 text-[#52321b] shadow-sm placeholder:text-[#bebab7] sm:text-sm sm:leading-6"
               />
