2f7906f3d4c1a90aea47fba7d46a73a60e675e8f
svivek19 <svivek030503@gmail.com>
2024-04-16 10:13:34 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-x-hidden">
       <Router>
         <Offer />
         <Navbar />
